3dee7666ce88655ea7c5995c300343bc8310772a
LinguisticMystic <aija.pastare@gmail.com>
2021-05-16 09:30:51 +0300

    Validation for array inputs added

--- app/Http/Requests/CVFormRequest.php
+++ app/Http/Requests/CVFormRequest.php
@@ -32,7 +32,25 @@ class CVFormRequest extends FormRequest
             'house_number' => 'required',
             'city' => 'required',
             'postal_code' => 'required',
-            'country' => 'required'
+            'country' => 'required',
+
+            'school' => 'required|array',
+            'school.*' => 'required',
+            'degree' => 'required|array',
+            'degree.*' => 'required',
+            'major' => 'required|array',
+            'major.*' => 'required',
+            'education_start_date' => 'required|array',
+            'education_start_date.*' => 'required',
+
+            'company' => 'required|array',
+            'company.*' => 'required',
+            'job_title' => 'required|array',
+            'job_title.*' => 'required',
+            'work_start_date' => 'required|array',
+            'work_start_date.*' => 'required',
+            'description' => 'required|array',
+            'description.*' => 'required'
         ];
     }
 
@@ -40,7 +58,17 @@ class CVFormRequest extends FormRequest
     {
         return [
             'fname.required' => 'The first name field is required.',
-            'lname.required' => 'The last name field is required.'
+            'lname.required' => 'The last name field is required.',
+
+            'school.*.required' => 'The school field is required.',
+            'degree.*.required' => 'The degree field is required.',
+            'major.*.required' => 'The major field is required.',
+            'education_start_date.*.required' => 'The education start date field is required.',
+
+            'company.*.required' => 'The company field is required.',
+            'job_title.*.required' => 'The job title field is required.',
+            'work_start_date.*.required' => 'The work start date field is required.',
+            'description.*.required' => 'The description field is required.'
         ];
     }
 }
